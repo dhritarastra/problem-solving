8c26c714a35e3324c04cb5413be736cdd47c81b0
dhritarastra <84006815+dhritarastra@users.noreply.github.com>
2021-05-18 19:12:22 +0700

    Create BinaryTreeDeepestNode.java
    Adds BST solution

---
+++ src/com/piyush/easy/BinaryTreeDeepestNode.java
@@ -0,0 +1,55 @@
+package com.piyush.easy;
+
+public class BinaryTreeDeepestNode {
+//	Node class definition
+	static class Node{
+		int data;
+		Node left, right;
+		
+		Node(int key){
+			data = key;
+			left = null;
+			right = null;
+		}
+	}
+
+//	maxLevel maintains the maximum level reached so far
+	static int maxLevel = -1;
+//	res contains the data for maxLevel above
+	static int res = -1;
+	
+	/*this function does an in-order traversal and
+	 * sets the maxLevel and res at runtime checking each
+	 * and every node in the BST
+	 * */ 
+	static int findDeepestNode(Node root, int level) {
+		if(root != null) {
+			
+			findDeepestNode(root.left, ++level);
+			if(level > maxLevel) {
+				maxLevel = level;
+				res = root.data;
+			}
+			findDeepestNode(root.right, level);
+			
+		}
+		return res;
+	}
+	
+//	Driver code
+	public static void main(String[] args) {
+		
+		Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        root.right.left.right = new Node(7);
+        root.right.right.right = new Node(8);
+        root.right.left.right.right = new Node(9);
+        System.out.println(findDeepestNode(root, 0));
+	}
+	
+	
+}
