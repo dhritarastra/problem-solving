6480a1632ff992f60225308d6e50c7ebed8ea227
dhritarastra <84006815+dhritarastra@users.noreply.github.com>
2021-05-19 11:38:00 +0700

    dejunks classes, spick and span activity

--- src/com/piyush/easy/BinaryTreeDeepestNode.java
+++ src/com/piyush/easy/BinaryTreeDeepestNode.java
@@ -1,44 +1,52 @@
 package com.piyush.easy;
 
-public class BinaryTreeDeepestNode {
-//	Node class definition
-	static class Node{
+/**
+ * @author Piyush
+ *
+ */
+public class BinaryTreeDeepestNode 
+{
+	//	Node class definition
+	static class Node
+	{
 		int data;
 		Node left, right;
 		
-		Node(int key){
+		Node(int key)
+		{
 			data = key;
 			left = null;
 			right = null;
 		}
 	}
 
-//	maxLevel maintains the maximum level reached so far
+	//	maxLevel maintains the maximum level reached so far
 	static int maxLevel = -1;
-//	res contains the data for maxLevel above
+	//	res contains the data for maxLevel above
 	static int res = -1;
 	
 	/*this function does an in-order traversal and
 	 * sets the maxLevel and res at runtime checking each
 	 * and every node in the BST
 	 * */ 
-	static int findDeepestNode(Node root, int level) {
-		if(root != null) {
-			
+	static int findDeepestNode(Node root, int level) 
+	{
+		if(root != null) 
+		{
 			findDeepestNode(root.left, ++level);
-			if(level > maxLevel) {
+			if(level > maxLevel) 
+			{
 				maxLevel = level;
 				res = root.data;
 			}
 			findDeepestNode(root.right, level);
-			
 		}
 		return res;
 	}
 	
-//	Driver code
-	public static void main(String[] args) {
-		
+	//	Driver code
+	public static void main(String[] args) 
+	{
 		Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
@@ -50,6 +58,4 @@ public class BinaryTreeDeepestNode {
         root.right.left.right.right = new Node(9);
         System.out.println(findDeepestNode(root, 0));
 	}
-	
-	
 }
--- src/com/piyush/easy/LargestProductOfThreeIntegersInAnArray.java
+++ src/com/piyush/easy/LargestProductOfThreeIntegersInAnArray.java
@@ -1,46 +1,56 @@
 package com.piyush.easy;
 
-public class LargestProductOfThreeIntegersInAnArray {
-
-	public static int findLargestSumOfThreeInts(int[] nums) {
+/**
+ * @author Piyush
+ *
+ */
+public class LargestProductOfThreeIntegersInAnArray 
+{
+	public static int findLargestSumOfThreeInts(int[] nums) 
+	{
 		int result = 0;
 		int min_1 = Integer.MAX_VALUE, min_2 = Integer.MIN_VALUE;
 		int max_1 = Integer.MIN_VALUE, max_2 = Integer.MIN_VALUE, max_3 = Integer.MIN_VALUE;
 		
-//		iterating over the numbers in the array
-		for(int n: nums) {
+		//	iterating over the numbers in the array
+		for(int n: nums) 
+		{
 			/*
 			 * finding out the max 3 and least 2 numbers (-ve only) the combination of
 			 * either max 3 numbers or max and least 2 -ve numbers can be the largest
 			 * product
 			 */
-			if(n < min_1) {
+			if(n < min_1) 
+			{
 				min_2 = min_1;
 				min_1 = n;
-			}else if(n < min_2) {
+			}else if(n < min_2) 
+			{
 				min_2 = n;
-			}else if(n > max_1) {
+			}else if(n > max_1) 
+			{
 				max_3 = max_2;
 				max_2 = max_1;
 				max_1 = n;
-			}else if(n > max_2) {
+			}else if(n > max_2) 
+			{
 				max_3 = max_2;
 				max_2 = n;
-			}else if(n > max_3) {
+			}else if(n > max_3) 
+			{
 				max_3 = n;
 			}
 			
 			result = Math.max((min_1 * min_2 * max_1), (max_3 * max_2 * max_1));
 		}
-
 		return result;
 	}
 	
-//	Driver code
-	public static void main(String[] args) {
+	//	Driver code
+	public static void main(String[] args) 
+	{
 		int[] nums = {1, 3, -11, -9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 4, 8};
 		int largestProduct = findLargestSumOfThreeInts(nums);
 		System.out.println(largestProduct);
 	}
-	
 }
